93814db866e0e0cdc640b325749cade960c94305
ofekefrat <ofekefrat.w@gmail.com>
2022-11-02 12:47:17 +0200

    af

---
+++ mmn11/q1/main.c
@@ -0,0 +1,6 @@
+//
+// Created by ofeke on 02/11/2022.
+//
+
+#include <stdio.h>
+
---
+++ mmn11/q1/main.h
@@ -0,0 +1,8 @@
+//
+// Created by ofeke on 02/11/2022.
+//
+
+#ifndef LABC_MAIN_H
+#define LABC_MAIN_H
+
+#endif //LABC_MAIN_H
