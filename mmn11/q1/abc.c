f1324bea44a15af3ce71a3e4268aa76377666a4c
ofekefrat <ofekefrat.w@gmail.com>
2022-11-20 11:48:02 +0200

    20/11 11:47
    Signed-off-by: ofekefrat <ofekefrat.w@gmail.com>

--- mmn11/q1/abc.c
+++ mmn11/q1/abc.c
@@ -1,5 +1,9 @@
 /*
-I assume the string's length is <=80
+ This program finds ascending alphabetic sequences of at least 3 letters within a string inputted through stdin,
+ and shortens the string by replacing each sequence with its first letter, a hyphen, and then the last letter.
+ The program also prints the inputted string before the changes and after.
+
+ Assumptions: I assume the string's length is <=80, and that spaces will not b
 */
 
 #include "abc.h"
@@ -10,12 +14,11 @@ int main() {
 
     char input[MAXLENGTH];
 
-    printf("Please enter a string literal: \n");
-    if (scanf("%80s", input)) {
-        printf("You entered: %s\n", input);
-        abc(input);
-        printf("Result: %s\n", input);
-    }
+    printf("Please enter a string: \n");
+    (scanf("%80s", input));
+    printf("You entered: %s\n", input);
+    abc(input);
+    printf("Result: %s\n", input);
 
     return 0;
 }
@@ -26,13 +29,13 @@ void abc(char* str) {
     char tempStr[MAXLENGTH]; /* a temporary string for manipulation purposes */
 
     for (i=1; i < strlen(str); i++) {
-        if (flag==0 && isalpha(str[i])) {
+        if (!flag && isalpha(str[i])) {
             if (str[i] == str[i-1]+1) {
                 hyphenInd = i;
                 flag = 1;
             }
         }
-        else if (flag==1) {
+        else if (flag) {
             if (str[i] == str[i-1]+1) {
                 if (i == strlen(str)-1 || str[i+1] != str[i]+1) {
                     strcpy(tempStr, str + i);/*using tempStr to keep the rest of the string*/
@@ -40,6 +43,7 @@ void abc(char* str) {
                     strcat(str, "-"); /*adding the hyphen*/
                     strcat(str, tempStr); /*adding the rest of the string back*/
                     flag = 0;
+                    i = hyphenInd;
                 }
             }
             else
