4c5b8e077995ca058448f9a773817d449c623eca
ofekefrat <ofekefrat.w@gmail.com>
2022-12-18 11:41:06 +0200

    18/12 11:40
    Signed-off-by: ofekefrat <ofekefrat.w@gmail.com>

--- mmn12/my_bcmp/my_bcmp.c
+++ mmn12/my_bcmp/my_bcmp.c
@@ -10,8 +10,8 @@
 
 int main() {
 
-    char input[STR_MAX_LENGTH];
-    int b1, b2, len;
+    char input[STR_MAX_LENGTH]; /* string from user */
+    int b1, b2, len; /* b1,b2: indexes from user. len: length to be tested (from user)*/
 
     printf("Please enter len\n");
     len = getNumberFromStdin();
@@ -36,7 +36,7 @@ int main() {
     return 0;
 }
 
-void inputCheck(char* str, int b1, int b2, int len) {
+void inputCheck(char* str, int b1, int b2, int len) { /* Checking if all the required input is valid */
 
     if (b1 > strlen(str)) {
         printf("ERROR: First index is out of string's bounds\n");
@@ -56,19 +56,19 @@ void inputCheck(char* str, int b1, int b2, int len) {
     }
 }
 
-int getNumberFromStdin() {
+int getNumberFromStdin() { /* Receiving an integer (and only an integer) from the user */
 
-    char str[INT_MAX_LENGTH];
-    char* trash;
+    char str[INT_MAX_LENGTH]; /* char array to test if everything will be read correctly */
+    char* trash; /* pointer to dump unnecessary value from strtol*/
     int i;
 
-    if (fgets(str, INT_MAX_LENGTH, stdin) == NULL) {
+    if (fgets(str, INT_MAX_LENGTH, stdin) == NULL) { /* Checking if input was received successfully */
         printf("Error handling input\n");
         exit(0);
     }
 
     for (i=0; i<strlen(str)-1; i++) {
-        if (!isdigit(str[i])) {
+        if (!isdigit(str[i])) { /* Checking if all characters from input are digits, otherwise it's not a (non-negative) integer */
             printf("ERROR: Not a non-negative decimal integer\n");
             exit(0);
         }
@@ -77,16 +77,16 @@ int getNumberFromStdin() {
     return strtol(str, &trash, DECIMAL_BASE);
 }
 
-int my_bcmp(const void* b1, const void* b2, int len) {
+int my_bcmp(const void* b1, const void* b2, int len) { /* the required test*/
 
     int i;
-    const char* pb1 = b1;
+    const char* pb1 = b1; /* type casting to access the values of items in array */
     const char* pb2 = b2;
-    size_t b1_size = strlen(pb1);
+    size_t b1_size = strlen(pb1); /* length of string from each index */
     size_t b2_size = strlen(pb2);
 
-    if ( (b1_size == 0 && b2_size == 0)
-        || len == 0)
+    if ( (b1_size == 0 && b2_size == 0) /* if both "strings" (from indexes) are empty, they are equal */
+        || len == 0) /* if we check 0 items, they are equal by default */
         return 0;
 
     for (i=0; i < b1_size && i < b2_size && i < len; i++) {
