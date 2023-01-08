cf64ba3ea709fd298d67f5764897f919e18d84ed
ofekefrat <ofekefrat.w@gmail.com>
2023-01-08 10:46:41 +0200

    08/01 10:46
    Signed-off-by: ofekefrat <ofekefrat.w@gmail.com>

--- mmn22/set.c
+++ mmn22/set.c
@@ -32,7 +32,6 @@ void print_set(pSet targetSet, int* nums) {
 /* read_set: method to redefine the set pointed to by the first argument, with the members of the temporary set
  * passed by getSetMembers in the second argument.*/
 void read_set(pSet targetSet, pSet temp) {
-
     int i;
     for (i=0; i < SECTIONS; i++)
         targetSet->sections[i].value = temp->sections[i].value;
